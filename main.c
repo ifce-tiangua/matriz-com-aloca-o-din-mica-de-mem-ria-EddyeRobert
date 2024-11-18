a9bef6a272e70348526148700ee6bc2cd759cad3
Eddye <eddyerobert@gmail.com>
2024-11-18 13:05:29 -0300

    Update main.c

--- main.c
+++ main.c
@@ -3,14 +3,13 @@
 
 int main() {
     int linhas, colunas;
-    scanf("%d", &linhas);
 
-    if (linhas == 0) {
+    scanf("%d %d", &linhas, &colunas);
+
+    if (linhas == 0 || colunas == 0) {
         printf("[matriz vazia]\n");
         return 0;
     }
-
-    scanf("%d", &colunas);
 
     int **matriz = (int **)malloc(linhas * sizeof(int *));
     for (int i = 0; i < linhas; i++) {
@@ -25,10 +24,7 @@ int main() {
 
     for (int i = 0; i < linhas; i++) {
         for (int j = 0; j < colunas; j++) {
-            if (j != 0) {
-                printf(" ");
-            }
-            printf("%d", matriz[i][j]);
+            printf("%d ", matriz[i][j]);
         }
         printf("\n");
     }
